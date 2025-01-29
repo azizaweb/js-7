52be38e1a9d4b35d6e9c7b3c02053f24b1fbe89a
azizaweb <azizaweb@mail.ru>
2025-01-29 21:44:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// let fruits = [5,7,6,1,3,9];
+// console.log(fruits.sort()) сортирует в правильном порядке
+
+// let fruits = [1,2,3];
+// console.log(fruits.reverse()) переворачивает
+
+// let user = ['Hello', 'Madina'];
+// console.log(user.join(' ielts ')) присоединяет
+
+// let user = ['1','2','3','4','5'];
+// console.log(user.splice(1,2,9)) 
+// console.log(user)
+
+// let woeds = ['Привет', 'как' , 'дела']
+// console.log(user.join(' как '))
+
+//  let user = ['1','2','3','4','5'];
+//  console.log(user.splice(1,3,100,200,4)) 
+//  console.log(user)
+//  2 удаляет 3 доб
+
+// const ismLar = ['davron','madina','murad','leyli']
+//  ismLar.forEach(function(ism){
+//     console.log(ism)
+// }) для каждого может менять
+
+// const ismLar = ['davron','madina','murad','leyli']
+//  ismLar.forEach(function(ism){
+//     const newIsm = ism.charAt(). toUpperCase() + ism.slice(1).toLowerCase() + 'bek';
+//     console.log(newIsm)
+// }) 
+
+let user = ['non','banan','mashina','asal','olma','aziza','gul'];
+ console.log(user.splice(3)) 
+ console.log(user)
+
